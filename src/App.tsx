036f715ab0c83de9aad846690363f070f1ad363b
André Borba Netto Assis <andrebnassis@Mac-mini-de-mac.local>
2022-01-25 13:41:55 -0300

    implement bubble speech image with text on its center

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fifty_cent from './resources/50cent.png'
+import speech_bubble from './resources/speechBubble_crop.png'
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="speech-bubble-container">
+  <img src={speech_bubble} alt="Snow"/>
+  <code className="centered">R$ 2,75</code>
+</div>
   );
 }
 
