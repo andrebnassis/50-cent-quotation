d1a152c54027619499a71825fb155c004a962038
André Borba Netto Assis <andrebnassis@Mac-mini-de-mac.local>
2022-01-25 14:07:40 -0300

    add 50cent image with speech bubble positioned on most top-left

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,32 @@ import fifty_cent from './resources/50cent.png'
 import speech_bubble from './resources/speechBubble_crop.png'
 import { Box } from '@mui/material';
 
+//https://stackoverflow.com/questions/48474/how-do-i-position-one-image-on-top-of-another-in-html
+
 function App() {
   return (
-<div className="speech-bubble-container">
-  <img src={speech_bubble} alt="Snow"/>
-  <code className="centered">R$ 2,75</code>
+<div style={{
+    position: 'relative'
+}}>
+  <img style={{
+    position: 'relative'
+  }} src={fifty_cent} alt="50cent"/>
+  <div style={{position: 'absolute', top: 0, left: 0, transform: 'translate(0%, 0%)'}}>  
+    <div style={{
+      position: 'relative',
+    }}
+    >
+      <img src={speech_bubble} alt="speech bubble"/>
+      <code style={{  
+        position: 'absolute',
+        top: '36%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'}}
+      >
+        R$ 2,75
+      </code>
+    </div>
+  </div>
 </div>
   );
 }
